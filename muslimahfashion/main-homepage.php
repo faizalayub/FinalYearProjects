3f86fc48d80c70a680f7ea4aae95d1d6458b5d22
Muhammad Faizal Bin Ayub <faizalayub29@gmail.com>
2023-04-27 12:31:20 +0800

    done final

--- muslimahfashion/main-homepage.php
+++ muslimahfashion/main-homepage.php
@@ -6,6 +6,9 @@
 		include 'config.php';
         include 'header.php';
     ?>
+
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/landing-page.css">
 </head>
 
 <body>
@@ -15,14 +18,63 @@
 		<div class="main">
 			<?php include 'top-navbar.php'; ?>
 
-			<main class="content">
+			<main class="content p-0">
 				<div class="container-fluid p-0">
-                    homepage
+
+					<div class="slider-area position-relative" style="height: 90vh;">
+						<div class="slider-active">
+							<!-- Single Slider -->
+							<div class="single-slider position-relative hero-overly slider-height  d-flex align-items-center" data-background="img/hero/h1_hero.jpeg">
+								<div class="container">
+									<div class="row">
+										<div class="col-xl-6 col-lg-6">
+											<div class="hero-caption">
+												<img class="rotateme" src="img/hero/hero-icon.png" alt="" data-animation="zoomIn" data-delay="1s">
+												<h1 data-animation="fadeInLeft" data-delay=".4s">We make cloths that suit you</h1>
+												<p data-animation="fadeInLeft" data-delay=".6s">Brand new & exclusively designed Muslimah fashion for your Muslimah wear collection. Free delivery in Malaysia available for all purchases of Muslimah fashion. Clothing. Bestsellers.</p>
+												<!-- Hero Btn -->
+												<a href="main-shop.php" class="btn btn-primary btn-lg" data-animation="fadeInLeft" data-delay=".8s">Shop Now</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!-- Left img -->
+								<div class="hero-img">
+									<img src="img/hero/h2_hero2.jpeg" alt="" data-animation="fadeInRight" data-transition-duration="5s">
+								</div>
+							</div>
+							<!-- Single Slider -->
+							<div class="single-slider position-relative hero-overly slider-height  d-flex align-items-center" data-background="img/hero/h1_hero.jpeg">
+								<div class="container">
+									<div class="row">
+										<div class="col-xl-6">
+											<div class="hero-caption">
+												<img class="rotateme" src="img/hero/hero-icon.png" alt="" data-animation="zoomIn" data-delay="1s">
+												<h1 data-animation="fadeInLeft" data-delay=".4s">We make cloths that suit you</h1>
+												<p data-animation="fadeInLeft" data-delay=".6s">Brand new & exclusively designed Muslimah fashion for your Muslimah wear collection. Free delivery in Malaysia available for all purchases of Muslimah fashion. Clothing. Bestsellers.</p>
+												<!-- Hero Btn -->
+												<a href="main-shop.php" class="btn btn-primary btn-lg" data-animation="fadeInLeft" data-delay=".8s">Shop Now</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!-- Left img -->
+								<div class="hero-img">
+									<img src="img/hero/h2_hero2.jpeg" alt="" data-animation="fadeInRight" data-transition-duration="5s">
+								</div>
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</main>
 
 			<?php include 'footer.php'; ?>
 		</div>
 	</div>
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/slick.min.js"></script>
+	<script src="js/landing-page.js"></script>
 </body>
 </html>
--- muslimahfashion/main-shop.php
+++ muslimahfashion/main-shop.php
@@ -21,15 +21,15 @@
 			<main class="content">
 				<div class="container-fluid p-0">
 
-					<!--#START Filter --->
+					<!--#START Content --->
 					<div class="row">
-						<div class="col-12">
+						<div class="col-4">
 							<div class="card">
 								<div class="card-body pt-3 pb-3">
 
 									<label class="fw-bold pb-2">Search:</label>
 
-									<form method="GET" class="d-flex gap-3">
+									<form method="GET" class="d-flex flex-column gap-3">
 										<select name="searchcategory" class="form-control">
 											<option value="">-- Select Category --</option>
 											<?php
@@ -53,100 +53,97 @@
 								</div>
 							</div>
 						</div>
-					</div>
-					<!--#END Filter --->
-											
-					<!--#START Product List --->
-					<div class="row">
-						<?php
-							$alreadypush = [];
-							$searchquery = '';
+						<div class="col-8 row">
+							<?php
+								$alreadypush = [];
+								$searchquery = '';
 
-							if(!empty($searchKey)){
-								$searchquery .= "name LIKE '%$searchKey%'";
-							}
-
-							if(!empty($searchcategory)){
 								if(!empty($searchKey)){
-									$searchquery .= " AND ";
+									$searchquery .= "name LIKE '%$searchKey%'";
 								}
 
-								$searchquery .= "category = ".$searchcategory;
-							}
+								if(!empty($searchcategory)){
+									if(!empty($searchKey)){
+										$searchquery .= " AND ";
+									}
 
-							if(empty($searchKey) && empty($searchcategory)){
-								$searchquery = '(1=1)';
-							}
+									$searchquery .= "category = ".$searchcategory;
+								}
 
-							$productPreview = fetchRows("SELECT * FROM menu WHERE $searchquery AND is_active = 1");
-							
-							if(!empty($productPreview)){
-								//# Collect Order Product
-								$productOrder = [];
-								$orderFetch = fetchRows("SELECT * FROM `user_order`");
+								if(empty($searchKey) && empty($searchcategory)){
+									$searchquery = '(1=1)';
+								}
 
-								foreach($orderFetch as $order){
-									$itemorder = json_decode($order['menu_id']);
+								$productPreview = fetchRows("SELECT * FROM menu WHERE $searchquery AND is_active = 1");
+								
+								if(!empty($productPreview)){
+									//# Collect Order Product
+									$productOrder = [];
+									$orderFetch = fetchRows("SELECT * FROM `user_order`");
 
-									foreach($itemorder as $o){
-										if(isset($productOrder[$o])){
-											$productOrder[$o] = $productOrder[$o].",".$o;
-										}else{
-											$productOrder[$o] = $o;
+									foreach($orderFetch as $order){
+										$itemorder = json_decode($order['menu_id']);
+
+										foreach($itemorder as $o){
+											if(isset($productOrder[$o])){
+												$productOrder[$o] = $productOrder[$o].",".$o;
+											}else{
+												$productOrder[$o] = $o;
+											}
+										} 
+									}
+
+									//# Loop Product
+									foreach($productPreview as $c){
+										$totalOrdered = 0;
+										$categoryname = fetchRow("SELECT * from category WHERE id = ".$c['category']);
+										$stockbalance = $c['in_stock'];
+
+										if(isset($productOrder[$c['id']])){
+											$totalOrdered = explode(',',$productOrder[$c['id']]);
+											$totalOrdered = count($totalOrdered);
 										}
-									} 
-								}
 
-								//# Loop Product
-								foreach($productPreview as $c){
-									$totalOrdered = 0;
-									$categoryname = fetchRow("SELECT * from category WHERE id = ".$c['category']);
-									$stockbalance = $c['in_stock'];
+										if(($stockbalance - $totalOrdered) <= 0){
+											continue;
+										}
 
-									if(isset($productOrder[$c['id']])){
-										$totalOrdered = explode(',',$productOrder[$c['id']]);
-										$totalOrdered = count($totalOrdered);
-									}
+										//# Product Card
+										if(!in_array($c['id'], $alreadypush)){
+											echo '
+												<div class="col-3">
+													<div class="card">
 
-									if(($stockbalance - $totalOrdered) <= 0){
-										continue;
-									}
+														<div class="card-body pt-3 pb-3">
+															<img src="images/'.$c['image'].'" class="img img-thumbnail img-responsive"/>
 
-									//# Product Card
-									if(!in_array($c['id'], $alreadypush)){
-										echo '
-											<div class="col-2">
-												<div class="card">
+															<div class="w-100 d-flex flex-column gap-2 pt-2 ">
+																<span class="w-100 fw-bold">'.$c['name'].'</span>
 
-													<div class="card-body pt-3 pb-3">
-														<img src="images/'.$c['image'].'" class="img img-thumbnail img-responsive"/>
+																<span class="w-100 text-nowrap">'.$categoryname['name'].'</span>
 
-														<div class="w-100 d-flex flex-column gap-2 pt-2 ">
-															<span class="w-100 fw-bold">'.$c['name'].'</span>
+																<span class="w-100 text-muted text-nowrap">Stock: '.($stockbalance - $totalOrdered).'</span>
 
-															<span class="w-100 text-nowrap">'.$categoryname['name'].'</span>
+																<span class="w-100 fst-italic text-success fw-bold text-nowrap">RM '.$c['price'].'</span>
 
-															<span class="w-100 text-muted text-nowrap">Stock: '.($stockbalance - $totalOrdered).'</span>
-
-															<span class="w-100 fst-italic text-success fw-bold text-nowrap">RM '.$c['price'].'</span>
-
-															<a href="./main-shop-addcart.php?id='.$c['id'].'" class="btn btn-primary text-nowrap">Add Cart</a>
+																<a href="./main-shop-addcart.php?id='.$c['id'].'" class="btn btn-primary text-nowrap">Add Cart</a>
+															</div>
 														</div>
+
 													</div>
-
 												</div>
-											</div>
-										';
+											';
 
-										$alreadypush[] = $c['id'];
+											$alreadypush[] = $c['id'];
+										}
 									}
+								}else{
+									echo 'No record found';
 								}
-							}else{
-								echo 'No record found';
-							}
-						?>
+							?>
+						</div>
 					</div>
-					<!--#END Product List --->
+					<!--#END Content --->
 
 				</div>
 			</main>
